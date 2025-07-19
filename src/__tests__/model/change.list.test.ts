125b7d972cd259f0a1af550d5020df095ac7c48d
Phill Janowski <philajan@pm.me>
2025-07-19 13:23:32 -0500

    fix tests

---
+++ src/__tests__/model/change.list.test.ts
@@ -0,0 +1,5 @@
+import {describe, test} from "vitest";
+
+describe('ChangeList', () => {
+    test('should return empty recipe node', async () => {})
+})
---
+++ src/__tests__/service/recipe.service.test.ts
@@ -0,0 +1,5 @@
+import {describe, test} from "vitest";
+
+describe('RecipeService', () => {
+    test('RecipeService should return an empty recipe node', async () => {})
+})
