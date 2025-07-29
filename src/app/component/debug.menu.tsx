7bff3c07c4a9b52807d24f83c01acf7ec772e4d7
Phill Janowski <philajan@pm.me>
2025-07-29 13:37:43 -0500

    refactor modal

--- src/app/component/debug.menu.tsx
+++ src/app/component/debug.menu.tsx
@@ -1,14 +1,11 @@
 import {RecipeService} from "@/app/service/recipe.service";
 import {NEAPOLITAN, NEW_HAVEN_STYLE, NY_STYLE, PAPA_JOHNS} from "@/app/static-data.pizza";
-import {RecipeFormHandle} from "@/app/component/form/recipe.form";
-import {useRef, useState} from "react";
-import {ModalDialog} from "@/app/component/modal.dialog";
-import RecipeFormV2 from "@/app/component/form/recipe.form.v2";
+import {useRef} from "react";
+import RecipeFormModal, {RecipeFormModalHandle} from "@/app/component/recipe.form.modal";
 
 const DebugMenu = () => {
-    const [showModal, setShowModal] = useState(false);
-    const toggleModal = () => setShowModal(!showModal);
-    const recipeFormRef = useRef<RecipeFormHandle>(null);
+    const recipeFormModal = useRef<RecipeFormModalHandle>(null);
+
     return (
         <div className="flex flex-row gap-4">
             <button onClick={() => {
@@ -18,15 +15,8 @@ const DebugMenu = () => {
                 RecipeService.saveRecipeNode(NEW_HAVEN_STYLE);
             }}>Load data</button>
             <button onClick={() => {RecipeService.deleteAll()}}>Delete All</button>
-            <button onClick={toggleModal}>Add New Recipe</button>
-            {showModal && (
-                <ModalDialog onClose={toggleModal} onConfirm={() => {
-                    recipeFormRef.current!.submitRecipeNode();
-                    toggleModal();
-                }}>
-                    <RecipeFormV2 ref={recipeFormRef}/>
-                </ModalDialog>
-            )}
+            <button onClick={() => {recipeFormModal.current!.toggle()}}> Add New Recipe</button>
+            <RecipeFormModal ref={recipeFormModal}/>
         </div>
 
     )
---
+++ src/app/component/recipe.form.modal.tsx
@@ -0,0 +1,42 @@
+import {ModalDialog} from "@/app/component/modal.dialog";
+import RecipeFormV2 from "@/app/component/form/recipe.form.v2";
+import {forwardRef, useImperativeHandle, useRef, useState} from "react";
+import {RecipeFormHandle} from "@/app/component/form/recipe.form";
+
+export interface RecipeFormModalHandle {
+   toggle: () => void;
+}
+
+interface RecipeFormModalProps {}
+
+const RecipeFormModal = forwardRef<RecipeFormModalHandle, RecipeFormModalProps>((props, ref) => {
+    const [showModal, setShowModal] = useState(false);
+    const toggleModal = () => setShowModal(!showModal);
+    const recipeFormRef = useRef<RecipeFormHandle>(null);
+
+    useImperativeHandle(ref, () => {
+        return {
+            toggle(): void {
+                console.log("toggled")
+                toggleModal();
+            }
+        }
+    });
+
+    return (
+        <div>
+            {showModal &&
+            <ModalDialog onClose={toggleModal} onConfirm={() => {
+                recipeFormRef.current!.submitRecipeNode();
+                toggleModal();
+            }}>
+                <RecipeFormV2 ref={recipeFormRef}/>
+            </ModalDialog>
+            }
+        </div>
+    )
+});
+
+RecipeFormModal.displayName = "RecipeFormModal";
+
+export default RecipeFormModal;
